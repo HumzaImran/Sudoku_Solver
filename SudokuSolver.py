64281d7ce4dce3da7f9b014535f4a4a3d478bd6f
HumzaImran <humza.imran@ryerson.ca>
2020-11-03 01:12:30 -0500

    logic written

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,5 +1,5 @@
 
-
+#creat sudoku board
 sudokuBorad = [
 	[6,0,0,1,0,0,0,0,2],
 	[8,0,1,0,9,0,0,0,0],
@@ -10,7 +10,28 @@ sudokuBorad = [
 	[0,0,0,0,7,0,0,0,0],
 	[0,6,0,0,3,1,0,5,0],
 	[7,0,2,5,4,0,6,0,3],
-	];
+	]
 
 
-print (sudokuBorad)
+
+#choices
+#delcared a list with all the possible numbers in sudoku
+possibleNumbers = [1,2,3,4,5,6,7,8,9]
+counter = 0
+#constrains 
+def checkBoard():
+	for nums in sudokuBorad:
+		counter =+ 1
+		print("break")
+		print (counter)
+		if counter < 2:
+			for num2 in nums:
+				print(num2)
+
+checkBoard()
+#does it break the row 
+#does it break the colunm
+#does it break the sub-grid 
+
+#goal is to fill in all 0s 
+# pre decalred sudokuBorad
